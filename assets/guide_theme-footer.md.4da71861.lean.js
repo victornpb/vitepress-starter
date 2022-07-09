f6e5a398dbf39953c0800396a93f37245a9603cc
victornpb <victornpb@users.noreply.github.com>
2022-07-09 21:33:02 +0000

    deploy: 127f7b84aa206c48e9745912baeff1a7f30d5e10

---
+++ assets/guide_theme-footer.md.4da71861.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as e}from"./app.0e1448ef.js";const C=JSON.parse('{"title":"Footer","description":"","frontmatter":{},"headers":[],"relativePath":"guide/theme-footer.md","lastUpdated":1657402355000}'),l={name:"guide/theme-footer.md"},p=e("",5),o=[p];function t(r,c,i,y,D,F){return n(),a("div",null,o)}var A=s(l,[["render",t]]);export{C as __pageData,A as default};
